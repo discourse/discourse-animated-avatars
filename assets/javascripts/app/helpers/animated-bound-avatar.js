61f8293255a502b3aaaa4f5ca842f45df0861a9b
Jeff Wong <awole20@gmail.com>
2021-12-08 22:30:11 -0800

    FIX: turn off animated avatar on profile if prefers reduced motion

--- assets/javascripts/app/helpers/animated-bound-avatar.js
+++ assets/javascripts/app/helpers/animated-bound-avatar.js
@@ -1,10 +1,11 @@
 import { get } from "@ember/object";
 import { htmlHelper } from "discourse-common/lib/helpers";
+import { prefersReducedMotion } from "discourse/lib/utilities";
 import boundAvatar from "discourse/helpers/bound-avatar";
 
 export default htmlHelper((user, size) => {
   const avatar = boundAvatar.compute([user, size], {});
-  if (user.animated_avatar != null) {
+  if (user.animated_avatar != null && !prefersReducedMotion()) {
     avatar.string = avatar.string.replace(/\.png/, ".gif");
   }
   return avatar;
