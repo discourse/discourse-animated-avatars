d6c3e9b561779a9d5f535cc339236f4c793ee0f9
Jeff Wong <awole20@gmail.com>
2022-03-14 11:47:09 -0700

    DEV: add tests

---
+++ test/javascripts/acceptance/animated-avatars-test.js
@@ -0,0 +1,22 @@
+import { acceptance } from "discourse/tests/helpers/qunit-helpers";
+import { visit } from "@ember/test-helpers";
+import { test } from "qunit";
+import Topics from "../fixtures/topic-fixtures";
+
+acceptance("Animated avatars topic tests", function (needs) {
+  needs.pretender((server, helper) => {
+    const topicPath = "/t/1000.json";
+    const topicResponse = Topics[topicPath];
+    server.get(topicPath, () => helper.response(topicResponse));
+  });
+
+  test("does not have animated class with no animated avatar", async function (assert) {
+    await visit("/t/-/280");
+    assert.notOk(exists(".animated-avatar"), "adds an animated-avatar class");
+  });
+
+  test("has animated class when animated avatar", async function (assert) {
+    await visit("/t/-/1000");
+    assert.ok(exists(".animated-avatar"), "adds an animated-avatar class");
+  });
+});
