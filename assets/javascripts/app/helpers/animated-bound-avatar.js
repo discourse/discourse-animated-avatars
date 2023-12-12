f5483f819a8b99c054c226974c359af5b583dbc3
Jarek Radosz <jradosz@gmail.com>
2023-12-12 17:25:37 +0100

    DEV: Update linting

--- assets/javascripts/app/helpers/animated-bound-avatar.js
+++ assets/javascripts/app/helpers/animated-bound-avatar.js
@@ -1,6 +1,6 @@
-import { htmlHelper } from "discourse-common/lib/helpers";
-import { prefersReducedMotion } from "discourse/lib/utilities";
 import boundAvatar from "discourse/helpers/bound-avatar";
+import { prefersReducedMotion } from "discourse/lib/utilities";
+import { htmlHelper } from "discourse-common/lib/helpers";
 
 export default htmlHelper((user, size) => {
   const avatar = boundAvatar(user, size);
