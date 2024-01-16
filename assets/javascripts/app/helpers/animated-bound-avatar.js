6cd0ce5862aa609b7526e69d753e737bf58a4003
Crius <claudio.vallesi@gmail.com>
2024-01-16 12:40:49 +0000

    add meaningful messages in case of issues with the avatar structure

--- assets/javascripts/app/helpers/animated-bound-avatar.js
+++ assets/javascripts/app/helpers/animated-bound-avatar.js
@@ -4,9 +4,16 @@ import { htmlHelper } from "discourse-common/lib/helpers";
 
 export default htmlHelper((user, size) => {
   const avatar = boundAvatar(user, size);
-  if (user.animated_avatar != null && !prefersReducedMotion()) {
-    console.log(avatar);
-    avatar.__string = avatar.__string.replace(/\.png/, ".gif");
+  if (avatar) {
+    if (user.animated_avatar != null && !prefersReducedMotion()) {
+      if (avatar.__string) {
+        avatar.__string = avatar.__string.replace(/\.png/, ".gif");
+      } else {
+        console.warn('discourse-animated-avatar: The avatar string variable has changed, probably after an update of Discourse. Falling back on static avatar. The object is now ', avatar);
+      }
+    }
+  } else {
+    console.warn('discourse-animated-avatar: Cannot find the avatar object, probably after an update of Discourse. Falling back on static avatar.');
   }
   return avatar;
 });
